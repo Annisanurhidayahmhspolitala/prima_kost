998d2bcc2e3abf088fe250ed92e1fc84473353e1
dickygans04 <dicky23@mhs.politala.ac.id>
2024-10-21 22:06:35 +0800

    tagihan

--- prima_kost_update/resources/views/tagihan/tampil.blade.php
+++ prima_kost_update/resources/views/tagihan/tampil.blade.php
@@ -2,52 +2,62 @@
 
 @section('konten')
 
-    <div class="d-flex">
+<div class="card">
+    <div class="card-header d-flex justify-content-between align-items-center">
         <h4>Data Tagihan</h4>
-        <div class="ms-auto">
-            <a class="btn btn-success" href="{{ route('tagihan.tambah') }}">Tambah Tagihan</a>
-        </div>
     </div>
 
-    <table class="table table-bordered text-center">
-        <tr>
-            <th>No</th>
-            <th>Id_Tagihan</th>
-            <th>Jumlah_tagihan</th>
-            <th>Tanggal_Tagihan</th>
-            <th>Tenggat_Tagihan</th>
-            <th>Id_Penghuni</th>
-            <th>Aksi</th>
-            <th>Pembayaran</th> <!-- Tambahkan kolom pembayaran -->
-        </tr>
-        @foreach ($tagihans as $no=>$data)
-        <tr>
-            <td>{{ $no+1 }}</td>
-            <td>{{ $data->id_tagihan }}</td>
-            <td>Rp {{ number_format($data->jumlah_tagihan, 0, ',', '.') }}</td>
-            <td>{{ $data->tanggal_tagihan }}</td>
-            <td>{{ $data->tenggat_tagihan }}</td>
-            <td>{{ $data->id_penghuni }}</td>
-            <td>
-                <div class="d-inline-flex">
-                <a href="{{ route('tagihan.edit', $data->id_tagihan) }}" class="btn btn-sm btn-warning me-1">
-                    <i class="fas fa-edit"></i></a>
-                <form action="{{ route('tagihan.delete', $data->id_tagihan) }}" method="post">
-                @csrf
-                <button class="btn btn-sm btn-danger" type="submit">
-                    <i class="fas fa-trash"></i>
-                </button>
-                </form>
-                </div>
-            </td>
-            <td>
-                <!-- Tombol untuk melakukan pembayaran -->
-                <a href="{{ route('tagihan.bayar', $data->id_tagihan) }}" class="btn btn-sm btn-primary">
-                    <i class="fas fa-money-bill-wave"></i> Bayar
-                </a>
-            </td>
-        </tr>
-        @endforeach
-    </table>
+    <div class="card-body">
+        <table class="table table-bordered text-center">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Id Tagihan</th>
+                    <th>Jumlah Tagihan</th>
+                    <th>Tanggal Tagihan</th>
+                    <th>Tenggat Tagihan</th>
+                    <th>Id Penghuni</th>
+                    <th>Aksi</th>
+                    <th>Pembayaran</th> <!-- Tambahan kolom pembayaran -->
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tagihans as $no=>$data)
+                <tr>
+                    <td>{{ $no+1 }}</td>
+                    <td>{{ $data->id_tagihan }}</td>
+                    <td>Rp {{ number_format($data->jumlah_tagihan, 0, ',', '.') }}</td>
+                    <td>{{ $data->tanggal_tagihan }}</td>
+                    <td>{{ $data->tenggat_tagihan }}</td>
+                    <td>{{ $data->id_penghuni }}</td>
+                    <td>
+                        <div class="d-inline-flex">
+                            <a href="{{ route('tagihan.edit', $data->id_tagihan) }}" class="btn btn-sm btn-warning me-1">
+                                <i class="fas fa-edit"></i>
+                            </a>
+                            <form action="{{ route('tagihan.delete', $data->id_tagihan) }}" method="post">
+                                @csrf
+                                <button class="btn btn-sm btn-danger" type="submit">
+                                    <i class="fas fa-trash"></i>
+                                </button>
+                            </form>
+                        </div>
+                    </td>
+                    <td>
+                        <!-- Tombol untuk melakukan pembayaran -->
+                        <a href="{{ route('tagihan.bayar', $data->id_tagihan) }}" class="btn btn-sm btn-primary">
+                            <i class="fas fa-money-bill-wave"></i> Bayar
+                        </a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+    <div class="card-footer text-start">
+        <a class="btn btn-success" href="{{ route('tagihan.tambah') }}">+ Tambah Tagihan</a>
+    </div>
+</div>
 
 @endsection
