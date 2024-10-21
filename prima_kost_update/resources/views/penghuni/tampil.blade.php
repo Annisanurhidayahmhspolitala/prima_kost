7887e5bb48681520722266ce01d3b1877d86a6f8
Annisa Nurhidayah <annisanurhidayah23@mhs.politala.ac.id>
2024-10-21 22:05:05 +0800

    Update tampil.blade.php

--- prima_kost_update/resources/views/penghuni/tampil.blade.php
+++ prima_kost_update/resources/views/penghuni/tampil.blade.php
@@ -2,49 +2,59 @@
 
 @section('konten')
 
-    <div class="d-flex">
+<div class="card">
+    <div class="card-header d-flex justify-content-between align-items-center">
         <h4>Data Penghuni</h4>
-        <div class="ms-auto">
-            <a class="btn btn-success" href="{{ route('penghuni.tambah') }}">Tambah Penghuni</a>
-        </div>
     </div>
 
-    <table class="table table-bordered text-center">
-        <tr>
-            <th>No</th>
-            <th>Id_Penghuni</th>
-            <th>Nama_Penghuni</th>
-            <th>NIK</th>
-            <th>No_Hp_Penghuni</th>
-            <th>Jenis_Kelamin</th>
-            <th>Id_Akun</th>
-            <th>Id_Kamar</th>
-            <th>Aksi</th>
-        </tr>
-        @foreach ($penghunis as $no=>$data)
-        <tr>
-            <td>{{ $no+1 }}</td>
-            <td>{{ $data->id_penghuni }}</td>
-            <td>{{ $data->nama_penghuni }}</td>
-            <td>{{ $data->nik }}</td>
-            <td>{{ $data->no_hp_penghuni }}</td>
-            <td>{{ $data->jenis_kelamin }}</td>
-            <td>{{ $data->id_akun }}</td>
-            <td>{{ $data->id_kamar }}</td>
-            <td>
-                <div class="d-inline-flex">
-                <a href="{{ route('penghuni.edit', $data->id_penghuni) }}" class="btn btn-sm btn-warning me-1">
-                    <i class="fas fa-edit"></i></a>
-                <form action="{{ route('penghuni.delete', $data->id_penghuni) }}" method="post">
-                @csrf
-                <button class="btn btn-sm btn-danger" type="submit">
-                    <i class="fas fa-trash"></i>
-                </button>
-                </form>
-                </div>
-            </td>
-        </tr>
-        @endforeach
-    </table>
+    <div class="card-body">
+        <table class="table table-bordered text-center">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Id Penghuni</th>
+                    <th>Nama Penghuni</th>
+                    <th>NIK</th>
+                    <th>No HP Penghuni</th>
+                    <th>Jenis Kelamin</th>
+                    <th>Id Akun</th>
+                    <th>Id Kamar</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($penghunis as $no=>$data)
+                <tr>
+                    <td>{{ $no+1 }}</td>
+                    <td>{{ $data->id_penghuni }}</td>
+                    <td>{{ $data->nama_penghuni }}</td>
+                    <td>{{ $data->nik }}</td>
+                    <td>{{ $data->no_hp_penghuni }}</td>
+                    <td>{{ $data->jenis_kelamin }}</td>
+                    <td>{{ $data->id_akun }}</td>
+                    <td>{{ $data->id_kamar }}</td>
+                    <td>
+                        <div class="d-inline-flex">
+                            <a href="{{ route('penghuni.edit', $data->id_penghuni) }}" class="btn btn-sm btn-warning me-1">
+                                <i class="fas fa-edit"></i>
+                            </a>
+                            <form action="{{ route('penghuni.delete', $data->id_penghuni) }}" method="post">
+                                @csrf
+                                <button class="btn btn-sm btn-danger" type="submit">
+                                    <i class="fas fa-trash"></i>
+                                </button>
+                            </form>
+                        </div>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+    <div class="card-footer text-start">
+        <a class="btn btn-success" href="{{ route('penghuni.tambah') }}">+ Tambah Penghuni</a>
+    </div>
+</div>
 
 @endsection
